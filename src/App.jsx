5bfd580c576471fbc5d1597df383ae892a9f8d95
d-zucho <danny.nico.dev@outlook.com>
2023-01-05 11:14:22 -0800

    start details section

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
 import './styles/App.css'
+import Details from './components/Details/Details'
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Hero />
+      <Details />
       <Footer />
     </div>
   )
---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,13 @@
+import OrangeKeyboard from '../../assets/orange_keyboard_desktop.png'
+
+const Details = () => {
+  return (
+    <div>
+      <div className='orange-keyboard'>
+        <img src={OrangeKeyboard} alt='Orange Keyboard' />
+      </div>
+    </div>
+  )
+}
+
+export default Details
